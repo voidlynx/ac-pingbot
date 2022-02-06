301b582a3366e3282daa1d0aad03f5f12437c509
Calamity34 <NikitaYoutube2008@icloud.com>
2022-02-06 15:04:43 +0500

    added some more commands, switched if to switchcase

--- bot.dart
+++ bot.dart
@@ -14,16 +14,55 @@ Future<void> main(List<String> arguments) async {
   final nick = 'pingbot';
   final pref = '!';
   var recv;
+  var send = new Map();
+  send['user'] = nick;
 
   // Connect to the server
   final socket = await Socket.connect(host, port);
   print('\/\/ $nick connected to $host at $port\n');
-  // Listen to the ping command
+
+  // Listen to commands
   socket.listen((v) {
     recv = json.decode(utf8.decode(v, allowMalformed: true).trim());
-    if (recv['msg'].split(' ')[0] == pref + 'ping') {
-      socket.add(utf8.encode(json.encode({"user": nick, "msg": "pong"})));
-      print('Responded to ${recv["user"]} executing ping');
+    if (recv['msg'].startsWith(pref) && recv['user'] != nick) {
+      switch (recv['msg'].split(' ')[0].substring(pref.length)) {
+        // what the fuck
+        case "ping":
+          {
+            print('ping recieved');
+            send['msg'] = "pong";
+            socket.add(utf8.encode(json.encode(send)));
+          }
+          break;
+
+        case "chungus":
+          {
+            print('chungus recieved');
+            send['msg'] = "the big one";
+            socket.add(utf8.encode(json.encode(send)));
+          }
+          break;
+
+        case "chungussize":
+          {
+            print('chungussize recieved');
+            if (recv['msg'].split(' ').length < 2) {
+              send['msg'] = 'usage: ${pref}chungussize <size>';
+              socket.add(utf8.encode(json.encode(send)));
+            } else {
+              send['msg'] =
+                  'the size of the chungus is ${recv["msg"].split(" ")[1]}';
+              socket.add(utf8.encode(json.encode(send)));
+            }
+          }
+          break;
+
+        default:
+          {
+            print('unknown command recieved');
+          }
+          break;
+      }
     }
   });
 }
